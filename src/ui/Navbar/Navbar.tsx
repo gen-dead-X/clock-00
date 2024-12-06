b5396825bcb8686eba08507ef673573c1d9fd03e
Joyrudra Biswas <joyrudra@itobuz.com>
2024-12-06 15:54:12 +0530

    fix : lint isuues

--- src/ui/Navbar/Navbar.tsx
+++ src/ui/Navbar/Navbar.tsx
@@ -1,5 +1,3 @@
-import ThemeToggleButton from '../ThemeToggleButton/ThemeToggleButton';
-
 export default function Navbar() {
   return (
     <div className="fixed z-30 flex w-screen justify-end px-5">{/* <ThemeToggleButton /> */}</div>
