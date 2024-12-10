7a01ac10319f5f6c681152a899c1706f1205d3d7
Joyrudra Biswas <joyrudra@itobuz.com>
2024-12-10 18:17:46 +0530

    fix: removed pagination buttons

--- src/ui/Global/HomeCarousel/HomeCarousel.tsx
+++ src/ui/Global/HomeCarousel/HomeCarousel.tsx
@@ -1,22 +1,21 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/bundle';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import DefaultTransparentClock from '@/ui/Clocks/DefaultTransparent/DefaultTransparentClock';
 import WeatherWrapper from '@/ui/Weather/WeatherWrapper/WeatherWrapper';
+import './HomeCarousel.scss';
 
 export default function HomeCarousel() {
   const pages = [<DefaultTransparentClock key={1} />, <WeatherWrapper key={2} />];
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="relative flex h-full w-full items-center justify-center">
       <Swiper
-        modules={[Navigation, Pagination]}
-        navigation
+        modules={[Pagination]}
         pagination={{ clickable: true }}
-        className="h-full"
+        className="h-full w-full"
         spaceBetween={50}
         slidesPerView={1}
       >
